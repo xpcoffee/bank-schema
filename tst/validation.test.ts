3d5be555826bcdd771c399a1b3fdcf57c8b741d9
Emerick Bosch <xpc.dev@gmail.com>
2019-09-22 13:34:37 +0200

    Add tests for the transaction validator

--- tst/validation.test.ts
+++ tst/validation.test.ts
@@ -1,19 +1,53 @@
 import {validateStatement, validateTransaction} from "../src/validation";
-import { validate } from "jsonschema";
 
 describe("validateStatement", () => {
     it("passes validation on a valid statement", () => {
         const result = validateStatement({
             "account": "foo",
             "bank": "bar",
-            "transactions": []
+            "transactions": [],
+            "unknown key": "should be ignored"
         }).valid;
         expect(result).toBeTruthy();
     });
 
     it("fails validation on an invalid statement", () => {
-        const result = validateStatement({ hello: "world" });
+        const result = validateStatement({
+            "account": 1,
+            "bank": 2,
+            "transactions": 3 
+        });
         expect(result.valid).toBeFalsy();
+    });
+
+    it("fails validation on an empty object", () => {
+        const result = validateStatement({});
+        expect(result.valid).toBeFalsy();
+    });
+})
+
+describe("validateTransaction", () => {
+    it("passes validation on a valid statement", () => {
+        const result = validateTransaction({
+            "hash": "foobarbaz1",
+            "timeStamp": "foo",
+            "amountInZAR": 1,
+            "description": "baz",
+            "balance": 1,
+            "unknown key": "should be ignored"
+        }).valid;
+        expect(result).toBeTruthy();
+    });
+
+    it("fails validation on an invalid statement", () => {
+        const result = validateTransaction({
+            "hash": 1,
+            "timeStamp": 2,
+            "amountInZAR": 3,
+            "description": 4,
+            "balance": "foo"
+        }).valid;
+        expect(result).toBeFalsy();
     });
 
     it("fails validation on an empty object", () => {
